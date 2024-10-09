a080b366f1872bd56a6a7183e95f818b566292fd
maryam <marychch19@gmail.com>
2024-10-09 19:05:04 +0330

    itemList

---
+++ task3/part3.js
@@ -0,0 +1,5 @@
+const itemList = document.querySelector(".itemList");
+const input = document.querySelector(".input");
+const clear = document.querySelector(".clear");
+
+itemList.addEventListener("click", function (even) {});
